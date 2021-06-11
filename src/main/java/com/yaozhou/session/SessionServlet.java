d7f47646164e5509fb5c0ec09b73e38962810275
HANG <wangxiao_h@126.com>
2021-06-11 10:33:30 +0800

    first

---
+++ src/main/java/com/yaozhou/session/SessionServlet.java
@@ -0,0 +1,23 @@
+package com.yaozhou.session;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Created by WXHang on HANG at 2021/6/11 10:29
+ * @author HANG
+ */
+public class SessionServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        doGet(req, resp);
+    }
+}
